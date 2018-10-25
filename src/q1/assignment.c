071a5cc952cdd477f025941ab3cd58dc1622a613
smurfy567 <smurf104@gmail.com>
2018-10-25 20:30:52 +0100

    Q1 updated

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -4,3 +4,25 @@ int main(int arg, char* argc[]){
   printf("Hello assignment1.\n");
   return 0;
 }
+
+
+int a;
+int* b;
+long c;
+double* d;
+char** e;
+
+
+
+int main(int arg, char* argc[]) {
+
+
+	printf("%lu \n", sizeof(a));
+	printf("%lu \n", sizeof(b));
+	printf("%lu \n", sizeof(c));
+	printf("%lu \n", sizeof(d));
+	printf("%lu \n", sizeof(e));
+
+	return 0;
+
+}
